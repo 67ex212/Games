ccdc107ffce1d54e4276fbdfe81aaf1c4cf2e3b5
Thomas Lee <Thomas Lee>
2017-04-20 16:59:47 -0700

    mine incrementation

--- Minesweeper.py
+++ Minesweeper.py
@@ -14,8 +14,14 @@ MINE = 1
 FLAG = 2
 REVEALED = 99
 '''
+
 def initialize():
 	def mineLocation(numberOfMines, rows, columns): #Creates a random string of 1's and 0's depending on how many mines the player wants
+		
+		numberOfMines = input int("Enter Number of Mines")
+		rows = input int("Enter Number of Rows")
+		columns = input int("Enter Number of columns")
+
 		veryLongList = []
 
 		lengthOfRandomList = rows * columns 		#creates list of length row*column
@@ -25,9 +31,8 @@ def initialize():
 			veryLongList[mine] = 1
 
 		fieldArray = np.asarray(lengthOfRandomList) 
-		r.shuffle(lengthOfRandomList)
-		np.reshape(lengthOfRandomList, (rows, columns))
-	
+		r.shuffle(fieldArray)
+		np.reshape(fieldArray, (rows, columns))
 
 class Cell(object):
 	def __init__(self, isMine, isFlag, isNotKnown, isNearMe):
@@ -37,42 +42,105 @@ class Cell(object):
 		self.isNearMe = isNearMe             # integer between 0 and 8
 
 	def isMine(coord):
-		if coordState = 1:
+		if coordState == 1:
 			return True
 		else:
 			return False
 
 	def isFlag(coord):
-		if coordState = 2:
+		if coordState == 2:
 			return True
 		else: 
 			return False
 
 	def isRevealed(coord):
-		if coordState = 99:	
+		if coordState == 99:	
 			return True
 		else:
 			return False
 
-	def isNearMe(coord):
-		for x in [-1, 0, 1]:
-			for y in [-1, 0, 1]:
+	def isNearMe(coord): #Coord is [Xpos, Ypos]
+	
+		Xpos = coord[0]
+		Ypos = coord[1]
+
+		mineIncrement = 0
+
+		if Xpos == 0 and Ypos == 0: #Top Left
+			for x in [0, 1]: 
+				for y in [0, 1]:	# 00 01 10 11
+					newCoord = [Xpos + x, Ypos + y]
+					
+					if isMine(newCoord) == True:
+						mineIncrement += 1
+					else:
+						mineIncrement += 0
+				return mineIncrement     #Finished
+		
+
+		if Xpos == 0 and Ypos == rows: #Bottom Left
+			for x in [0, 1]:
+				for y in [-1, 0]:   # 0-1 00 1-1 10
+					newCoord = [Xpos + x, Ypos + y]
+
+					if isMine(newCoord) = True:
+						mineIncrement += 1
+				 	else:
+				 		mineIncrement += 0
+				return mineIncrement #finished
+
+
+		if Xpos == columns and Ypos == 0: #Top Right
+			for x in [-1, 0]:
+				for y in [0, 1]:
+					newCoord = [Xpos + x, Ypos + y]
+
+					if isMine(newCoord) = true:
+						mineIncrement += 1
+					else:
+						mineIncrement += 0
+				return mineIncrement #finsished
+
+
+		if Xpos == columns and Ypos == rows: #Bottom Right
+			for x in [-1, 0]:
+				for y in [-1, 0]:
+					newCoord = [Xpos + x, Ypos + y]
+
+					if isMine(newCoord) = true:
+						mineIncrement += 1
+					else:
+						mineIncrement += 0
+
+
+		if 
+
+		else:
+			for x in [-1, 0, 1]:
+				for y in [-1, 0, 1]:
+					if isMine(newCoord) == true
+
 
 '''
-So far we have...
+So far we have... 
 
--Made a random array consisting of mines
-and empty cells with the initialize Function
+-Made a random array consisting of mines 
+and empty cells with the initialize Function 
 
--Defined the Cell object and its properties
-that we will need to use
+-Defined the Cell object and its properties 
+that we will need to use 
 
 '''
 
-class Player(object):
+class MineField(object):
+
 	def __init__(self, isSolved, isDead):
 		self.isSolved = isSolved
 		self.isDead = isDead
+		self.x = Xloc
+		self.y = Yloc
+		self.minesLeft
+
 
 	def isDead(coordState):
 		if self.isMine = True and self.isRevealed = True:
@@ -81,18 +149,7 @@ class Player(object):
 			returrn False
 
 	def isSolved(fieldState):
-		for cells in fieldState:
-
-		
-	
-
-class Square(object):
-
-	def isDead():
-
-	def 
-	
-	
+		for cells in 
 
 
 
