077019d987fc7e242b15b3515ef5a395c942195f
67ex212 <tomylee091600@gmail.com>
2018-02-25 00:11:47 -0800

    Add files via upload
    im ded

---
+++ Career Game.py
@@ -0,0 +1,16 @@
+
+
+while GameEnd = False:
+	Print("Welcome to Google!")
+	Print("You'll be working here and trying to make your way up!")
+	Print("Here are the commands you can use for the month")
+	print("Work: You spend your month entirely working")
+	print("Rest: Earn back some Stamina")
+	Print("Retire: Collect savings and end the game")
+	Print("Move: Switch to a new company. You lose benefits,") 
+	Print("      but get better pay")
+	print("Quit: Immediately quit the game")
+	Print("You can always bring this panel back up by typing help")
+
+	
+
